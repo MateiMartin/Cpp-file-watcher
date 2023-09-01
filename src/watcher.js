5309aa0fdf7ee90d363a0b6b00a841415e344c4d
Matei Martin <126075907+MateiMartin@users.noreply.github.com>
2023-09-01 18:57:44 +0300

    Add files via upload

---
+++ src/watcher.js
@@ -0,0 +1,77 @@
+#!/usr/bin/env node
+const { time } = require('console');
+const fs = require('fs');
+const spawn = require('child_process').spawn;
+const args = process.argv.slice(2);
+
+const clearConsole = () => {
+    process.stdout.write('\x1Bc');
+    process.stderr.write('\x1Bc');
+};
+
+const app = {
+    /**
+     * Checks if a filename was passed as command
+     * line arguments.
+     */
+    init: function () {
+        that = this;
+        if (args[0] === undefined || args[0] === "") {
+            console.error("Error: No filename passed, exiting...");
+            return;
+        }
+        const file_name = './' + args[0];
+        that.watch_file(file_name);
+    },
+    /**
+     * Watches .cpp file for changes.
+     */
+    watch_file: function (file_name) {
+        let fswait = false;
+        console.log(`Watching process started for file ${file_name}`);
+        let ext = args[0].length - 4;
+        let output_file = args[0].substring(0, ext);
+        fs.watch(file_name, (event, filename) => {
+            if (filename) {
+                if (fswait) return;
+                fswait = setTimeout(() => {
+                    fswait = false;
+                }, 100);
+                const compile_file = spawn('g++', ["-o", output_file, file_name]);
+
+                compile_file.stdout.on('data', function (data) {
+                    console.log(`stdout: ${data}`);
+                    console.log(`${output_file} object file successfully generated`);
+                });
+
+                compile_file.stderr.on('data', function (data) {
+                    console.log(`stderr: ${data}`);
+                });
+
+                compile_file.on('close', (code) => {
+                    if (code === 0) {
+                        clearConsole();
+                        setTimeout(() => {
+                            const run_program = spawn(`./${output_file}`);
+                            run_program.stdout.on('data', function (data) {
+                                clearConsole();
+                                console.log(`Code output at ${new Date().toLocaleTimeString()}:`);
+                                console.log('\x1b[36m', `${data}`);
+                            });
+
+                        }, 100);
+
+
+                    } else {
+                        console.error(`Compilation failed with code ${code}`);
+                    }
+                });
+            }
+        });
+    }
+}
+
+// Starts the application
+app.init();
+
+module.exports = app;
